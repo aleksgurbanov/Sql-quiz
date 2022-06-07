8708a55a78671f3d9899389d54729f006c1071cd
Aliskandar Gurbanov <98799140+aleksgurbanov@users.noreply.github.com>
2022-06-07 11:11:47 +0400

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE Academy
+
+USE Academy
+
+
+Create Table Groups (ID int, Name nvarchar(255))
+
+Create Table Students (ID int, Name nvarchar(255) , Surname nvarchar(255) , GroupId int)
